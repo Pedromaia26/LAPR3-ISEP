e7041caed949a85f47a108e9c6d30dc9dff0831b
LAPTOP-042NG862\Tiago <1201276@isep.ipp.pt>
2021-11-07 02:12:38 +0000

    G41-8 #time 1h #comment adding controller class and updating main class to verify US107 implementation

---
+++ src/main/java/lapr/project/controller/PairController.java
@@ -0,0 +1,104 @@
+package lapr.project.controller;
+
+import lapr.project.model.Company;
+import lapr.project.model.Ship;
+
+import java.util.*;
+
+public class PairController {
+
+    public void returnPair (Company company){
+
+        List<Ship> shipList = (List<Ship>) company.getBstShips().inOrder();
+        Map<Ship, List<Ship>> pair = new LinkedHashMap<>();
+        float latD1, lngD1, latA1, lngA1, latD2, lngD2, latA2, lngA2, departureDistance, arrivalDistance, dist1, dist2, dist3;
+
+        for (int i = 0; i < shipList.size()-1; i++) {
+
+            boolean exists = false;
+            List<Ship> teste = new ArrayList<>();
+            List<Float> distances = new ArrayList<>();
+
+            for (int j = i+1; j < shipList.size() ; j++) {
+
+                System.out.println("------------");
+                System.out.println(shipList.get(i).getMmsi()  + "/" + shipList.get(j).getMmsi());
+
+                latD1 = Float.parseFloat(shipList.get(i).getBstDynData().departure().getLatitude());
+                lngD1 = Float.parseFloat(shipList.get(i).getBstDynData().departure().getLongitude());
+                latA1 = Float.parseFloat(shipList.get(i).getBstDynData().arrival().getLatitude());
+                lngA1 = Float.parseFloat(shipList.get(i).getBstDynData().arrival().getLongitude());
+
+                latD2 = Float.parseFloat(shipList.get(j).getBstDynData().departure().getLatitude());
+                lngD2 = Float.parseFloat(shipList.get(j).getBstDynData().departure().getLongitude());
+                latA2 = Float.parseFloat(shipList.get(j).getBstDynData().arrival().getLatitude());
+                lngA2 = Float.parseFloat(shipList.get(j).getBstDynData().arrival().getLongitude());
+
+
+                dist1 = shipList.get(i).getBstDynData().travelledDistance(latD1, lngD1, latA1, lngA1);
+                System.out.printf("Travelled distance between departure and arrival (Ship1): %.0f m\n", dist1);
+
+                if (dist1 < 10000){
+                    System.out.println("Ship1 travelled less than 10 km.");
+                    break;
+                }
+
+                dist2 = shipList.get(i).getBstDynData().travelledDistance(latD2, lngD2, latA2, lngA2);
+                System.out.printf("Travelled distance between departure and arrival (Ship2): %.0f m\n", dist2);
+
+                if (dist2 < 10000){
+                    System.out.println("Ship2 travelled less than 10 km.");
+                    continue;
+                }
+
+
+                dist3 = Math.abs(dist2-dist1);
+                System.out.println("Distance between ships: " + dist3 + "km");
+
+                departureDistance = shipList.get(i).getBstDynData().travelledDistance(latD1, lngD1, latD2, lngD2);
+                arrivalDistance = shipList.get(i).getBstDynData().travelledDistance(latA1, lngA1, latA2, lngA2);
+
+                if (departureDistance < 5000000 || arrivalDistance < 5000000) {
+
+                    distances.add(dist3);
+                    teste.add(shipList.get(j));
+
+                    System.out.println("Close departure or arrival: " + shipList.get(i) + " ----- " + shipList.get(j));
+
+                    exists = true;
+
+                }else
+                    System.out.println("Distant coordinates between departures/arrivals");
+
+            }
+
+
+
+            if (exists == true){
+                for(int l=0; l < distances.size(); l++){
+                    for(int j=1; j < distances.size()-l; j++){
+                        if(distances.get(j-1) < distances.get(j)){
+                            Collections.swap(distances, j-1, j);
+                            Collections.swap(teste, j-1, j);
+                        }
+                    }
+                }
+                pair.put(shipList.get(i), teste);
+            }
+
+
+            if (i==shipList.size()-2 && teste!=null){
+                System.out.println("\nPairs of ships with routes with close departure/arrival coordinates:\n");
+
+
+                for(Ship key : pair.keySet()) {
+                    for (Ship value : pair.get(key)) {
+                        System.out.println(key + " / " + value);
+                    }
+                }
+            }
+
+        }
+
+    }
+}
--- src/main/java/lapr/project/ui/Main.java
+++ src/main/java/lapr/project/ui/Main.java
@@ -1,6 +1,9 @@
 package lapr.project.ui;
 
-import lapr.project.model.CalculatorExample;
+import lapr.project.controller.ImportShipsController;
+import lapr.project.controller.PairController;
+import lapr.project.model.Company;
+import lapr.project.model.Ship;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -30,12 +33,20 @@ class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException, SQLException {
-        CalculatorExample calculatorExample = new CalculatorExample();
-        int value = calculatorExample.sum(3, 5);
+        String file = "sships.csv";
+        ImportShipsController controller = new ImportShipsController();
+        PairController pController = new PairController();
+        Company c = new Company();
+        controller.importFromCSV(file, c);
+        pController.returnPair(c);
 
-        if (LOGGER.isLoggable(Level.INFO)) {
+
+
+
+
+        /*if (LOGGER.isLoggable(Level.INFO)) {
             LOGGER.log(Level.INFO, String.valueOf(value));
-        }
+        }*/
     }
 }
 
