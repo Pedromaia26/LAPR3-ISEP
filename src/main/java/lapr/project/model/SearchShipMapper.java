67ed9fc43b57a35bbefe9cc45071c3dc2bc48497
Pedro_Maia <1201381@isep.ipp.pt>
2021-11-08 15:29:49 +0000

    G41-5 #time 10min #comment Adding the SearchShipMapper constructor and testing

--- src/main/java/lapr/project/model/SearchShipMapper.java
+++ src/main/java/lapr/project/model/SearchShipMapper.java
@@ -1,9 +1,9 @@
 package lapr.project.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class SearchShipMapper{
+
+    public SearchShipMapper() {
+    }
 
     public static SearchShipDto toDto(Ship ship) {
         return new SearchShipDto(ship);
--- src/test/java/lapr/project/model/SearchShipMapperTest.java
+++ src/test/java/lapr/project/model/SearchShipMapperTest.java
@@ -8,6 +8,7 @@ class SearchShipMapperTest {
 
     @Test
     void toDto() {
+        SearchShipMapper mapper = new SearchShipMapper();
         Ship ship = new Ship("123456789", "ship", "1000000000", "callSign", "A", "100", "500", "3");
         SearchShipDto expected = new SearchShipDto(ship);
         SearchShipDto valor = SearchShipMapper.toDto(ship);
