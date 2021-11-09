cab1493c90220bceb2fc8065c408c2b43f2ae6ed
Tiago Marques <1201276@isep.ipp.pt>
2021-11-09 22:38:47 +0000

    G4-4 #time 30min #comment killling alive mutants in ShipDynData class

--- src/test/java/lapr/project/model/ShipDynDataTest.java
+++ src/test/java/lapr/project/model/ShipDynDataTest.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+
 public class ShipDynDataTest {
 
     ShipDynData sdd = new ShipDynData("31/12/2020 19:25", "-66.97000", "22.81780",  "14.4", "11.2", "347", "NA", "B");
@@ -50,5 +51,71 @@ public class ShipDynDataTest {
         Date baseDateTime = sdd.getBaseDateTime();
         assertEquals(expectedBaseDateTime2, baseDateTime);
     }
+
+
+    @Test
+    public void latitudeUnderNegative90() throws ParseException {
+        ShipDynData sdd = new ShipDynData("31/12/2020 19:25", "-96.97000", "22.81780",  "14.4", "11.2", "347", "NA", "B");;
+        String expectedLatitude = "NA";
+        String latitude = sdd.getLatitude();
+        assertEquals(expectedLatitude, latitude);
+    }
+
+    @Test
+    public void latitudeOver90() throws ParseException {
+        ShipDynData sdd = new ShipDynData("31/12/2020 19:25", "96.97000", "22.81780",  "14.4", "11.2", "347", "NA", "B");;
+        String expectedLatitude = "NA";
+        String latitude = sdd.getLatitude();
+        assertEquals(expectedLatitude, latitude);
+    }
+
+    @Test
+    public void longitudeUnderNegative180() throws ParseException {
+        ShipDynData sdd = new ShipDynData("31/12/2020 19:25", "76.97000", "-192.81780",  "14.4", "11.2", "347", "NA", "B");;
+        String expectedLongitude = "NA";
+        String longitude = sdd.getLongitude();
+        assertEquals(expectedLongitude, longitude);
+    }
+    @Test
+    public void longitudeOver180() throws ParseException {
+        ShipDynData sdd = new ShipDynData("31/12/2020 19:25", "76.97000", "200.13121",  "14.4", "11.2", "347", "NA", "B");;
+        String expectedLongitude = "NA";
+        String longitude = sdd.getLongitude();
+        assertEquals(expectedLongitude, longitude);
+    }
+    @Test
+    public void cogUnder0() throws ParseException {
+        ShipDynData sdd = new ShipDynData("31/12/2020 19:25", "76.97000", "22.13121",  "14.4", "-4.0", "347", "NA", "B");;
+        float expectedCog = 356f;
+        float cog = sdd.getCog();
+        assertEquals(expectedCog, cog);
+    }
+    @Test
+    public void cogOver359() throws ParseException {
+        ShipDynData sdd = new ShipDynData("31/12/2020 19:25", "76.97000", "22.13121",  "14.4", "374.0", "347", "NA", "B");;
+        float expectedCog = 14f;
+        float cog = sdd.getCog();
+        assertEquals(expectedCog, cog);
+    }
+
+    @Test
+    public void headingUnder0() throws ParseException {
+        ShipDynData sdd = new ShipDynData("31/12/2020 19:25", "76.97000", "22.13121",  "14.4", "11.2", "-10", "NA", "B");;
+        String expectedHeading = "NA";
+        String heading = sdd.getHeading();
+        assertEquals(expectedHeading, heading);
+
+    }
+    @Test
+    public void headingOver359() throws ParseException {
+        ShipDynData sdd = new ShipDynData("31/12/2020 19:25", "76.97000", "22.13121",  "14.4", "11.2", "-10", "NA", "B");;
+        String expectedHeading = "NA";
+        String heading = sdd.getHeading();
+        assertEquals(expectedHeading, heading);
+
+    }
+
+
+
 
 }
