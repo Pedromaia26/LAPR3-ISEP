0f047bde69a23eb5c487336ec379fea325d1c38c
Pedro_Maia <1201381@isep.ipp.pt>
2021-11-10 11:19:32 +0000

    G41-4 #time 15min #comment Adding unit tests to distFrom method in BSTDynData class

--- src/test/java/lapr/project/model/BSTDynDataTest.java
+++ src/test/java/lapr/project/model/BSTDynDataTest.java
@@ -176,4 +176,33 @@ class BSTDynDataTest {
         expected += date3 + "\tLATITUDE: -66.97000; LONGITUDE: 22.81780\n";
         assertEquals(expected, bst.toString());
     }
+
+    @Test
+    void distFrom() {
+        double lat1 = -66.97000;
+        double lon1 = 22.81780;
+        double lat2 = -66.98000;
+        double lon2 = 22.81790;
+        double expected = 1111.957763671875;
+        assertEquals(expected, bst.distFrom(lat1,lon1,lat2,lon2));
+    }
+
+    @Test
+    void distFromSameLocal() {
+        double lat1 = -66.97000;
+        double lon1 = 22.81780;
+        double lat2 = -66.97000;
+        double lon2 = 22.81780;
+        double expected = 0;
+        assertEquals(expected, bst.distFrom(lat1,lon1,lat2,lon2));
+    }
+
+    @Test
+    void distFrom2Locals() {
+        double lat1 = -66.98000;
+        double lon1 = 22.81790;
+        double lat2 = -66.97000;
+        double lon2 = 22.81780;
+        assertEquals(bst.distFrom(lat2,lon2,lat1,lon1), bst.distFrom(lat1,lon1,lat2,lon2));
+    }
 }
