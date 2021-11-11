9b8d418b83a01748dfd804e532f3492c4e450b4e
Pedro_Maia <1201381@isep.ipp.pt>
2021-11-11 03:35:38 +0000

    G41-7 #time 30min #comment Adding the usage of company and outputing to a external file

--- src/main/java/lapr/project/controller/TopNshipsPerKmController.java
+++ src/main/java/lapr/project/controller/TopNshipsPerKmController.java
@@ -2,19 +2,32 @@ package lapr.project.controller;
 
 import lapr.project.model.BSTDynData;
 import lapr.project.model.BSTShip;
+import lapr.project.model.Company;
 import lapr.project.model.Ship;
+import lapr.project.utils.FileOperation;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 public class TopNshipsPerKmController {
 
     BSTDynData dataBst = new BSTDynData();
-    BSTShip shipBst = new BSTShip();
+    BSTShip shipBst;
 
-    public Map<Ship, Double> shipsAndKm() {
+    public TopNshipsPerKmController() {
+        shipBst = App.getInstance().getCompany().getBstShips();
+    }
+
+    public TopNshipsPerKmController(Company c) {
+        shipBst = c.getBstShips();
+    }
+
+    public HashMap<Ship, Double> shipsAndKm() {
 
         double totalKm = 0;
-        Map<Ship, Double> map = new HashMap<>();
+        HashMap<Ship, Double> map = new HashMap<>();
 
         Iterable<Ship> ships = shipBst.inOrder();
         Iterable<BSTDynData> viagens = dataBst.inOrder();
@@ -29,18 +42,35 @@ public class TopNshipsPerKmController {
         return map;
     }
 
-    public void printNshipsMostKm(int n, HashMap<Ship, Double> map){
-        if(n<=0){
-            throw new IllegalArgumentException("Parametrer invalid");
-        }
-        Map<Ship, Double> orderMap = sortByValue(map);
-        int i =0;
-            for (Map.Entry<Ship, Double> me : orderMap.entrySet()) {
-                while(i<n) {
-                System.out.println("Key = " + me.getKey() + ", Value = " + me.getValue());
-                i++;
+    public void printNshipsMostKm(String path) throws IOException {
+        String data = "";
+        HashMap<Ship, Double> map = shipsAndKm();
+        String line = "";
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        try {
+            line = br.readLine();
+            if (line != null) {
+                int n = Integer.parseInt(line);
+                if(n<=0){
+                    throw new IllegalArgumentException("Parameter invalid");
                 }
+                Map<Ship, Double> orderMap = sortByValue(map);
+                int i =0;
+                for (Map.Entry<Ship, Double> me : orderMap.entrySet()) {
+                    while(i<n) {
+                        data+="Key = " + me.getKey() + ", Value = " + me.getValue() + "\n";
+                        i++;
+                    }
+                }
+                FileOperation.writeToAFile("Output/TopNShips.txt", data);
             }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            br.close();
+        }
     }
 
     public static HashMap<Ship, Double> sortByValue(HashMap<Ship, Double> hm)
