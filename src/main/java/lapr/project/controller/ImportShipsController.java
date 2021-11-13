b078847ef0509629de1bebe172e8a624894c90c4
Pedro_Maia <1201381@isep.ipp.pt>
2021-11-13 12:53:44 +0000

    G41-2 #time 15min #comment Optimazing the insert of ships

--- src/main/java/lapr/project/controller/ImportShipsController.java
+++ src/main/java/lapr/project/controller/ImportShipsController.java
@@ -31,30 +31,18 @@ public class ImportShipsController {
                 String[] shipData = line.split(splitBy);
                 Ship ship = new Ship(shipData[0], shipData[7], shipData[8], shipData[9], shipData[10], shipData[11], shipData[12], shipData[13]);
                 ShipDynData dynamicData = new ShipDynData(shipData[1], shipData[2], shipData[3], shipData[4], shipData[5], shipData[6], shipData[14], shipData[15]);
-                if (!(shipBST.find(ship) == null)){
-                    if((shipBST.find(ship).getMmsi() == ship.getMmsi())){
-                        shipdyndataBST = shipBST.find(ship).getBstDynData();
-                        shipdyndataBST.insert(dynamicData);
-                        shipBST.find(ship).setBstDynData(shipdyndataBST);
-                    }
-                    else{
-                        shipdyndataBST = new BSTDynData();
-                        shipdyndataBST.insert(dynamicData);
-                        ship.setBstDynData(shipdyndataBST);
-                        shipBST.insert(ship);
-                    }
-                }
-                else{
+                if (shipBST.find(ship) == null) {
                     shipdyndataBST = new BSTDynData();
                     shipdyndataBST.insert(dynamicData);
                     ship.setBstDynData(shipdyndataBST);
                     shipBST.insert(ship);
+                }else {
+                    shipdyndataBST = shipBST.find(ship).getBstDynData();
+                    shipdyndataBST.insert(dynamicData);
+                    shipBST.find(ship).setBstDynData(shipdyndataBST);
                 }
                 line = br.readLine();
             }
-            //Iterable<Ship> it = shipBST.inOrder();
-            //System.out.println(it);
-
         } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
