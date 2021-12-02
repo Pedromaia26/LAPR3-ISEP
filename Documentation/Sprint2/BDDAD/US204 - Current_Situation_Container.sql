ed89c3b8966ab8a11947e5ab09ccce28d4cea2ee
Hugo Nogueira <1170504@isep.ipp.pt>
2021-12-02 22:47:10 +0000

    G41-68 - Update Script for user story 204

--- Documentation/Sprint2/BDDAD/US204 - Current_Situation_Container.sql
+++ Documentation/Sprint2/BDDAD/US204 - Current_Situation_Container.sql
@@ -1,9 +1,19 @@
-Select cml.status, s.name, p.name from Container c
-inner join (select c.id,ccm.id, max(ccm.date) AS LastRegist from Container_cargoManifest ccm
-inner join Container c on ccm.id = c.id
-group by c.id, ccm.id)
-inner join Cargo_Manifest_Load cml on cml.id = Container_cargoManifest.cargo_manifest_id
-inner join Ship s on cml.ship_mmsi = s.mmsi
-inner join Port p on cml.port_id = p.id
-where c.id = 3
-order by cml.status, s.name, p.name;
+CREATE OR REPLACE FUNCTION fncCurrentSituationContainer(ID_CONTAINER INT) 
+RETURN sys_refcursor IS 
+	currentSituationContainer sys_refcursor;
+BEGIN
+	OPEN currentSituationContainer FOR
+		SELECT c.id, cml.status, s.name, p.name from Container c
+		INNER JOIN (SELECT c.id,ccm.id, max(ccm.date) AS LastRegist from Container_cargoManifest ccm
+		INNER JOIN Container c on ccm.id = c.id
+		GROUP BY c.id, ccm.id)
+		INNER JOIN Cargo_Manifest_Load cml on cml.id = Container_cargoManifest.cargo_manifest_id
+		INNER JOIN Ship s on cml.ship_mmsi = s.mmsi
+		INNER JOIN Port p on cml.port_id = p.id
+		WHERE c.id = ID_CONTAINER
+		ORDER BY c.id, cml.status, s.name, p.name;
+
+
+	RETURN currentSituationContainer;
+END;
+/
