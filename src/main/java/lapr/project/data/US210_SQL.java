f7770a9fe0c6d3ebafaaacda0d5c676680b40ec1
Pedro_Maia <1201381@isep.ipp.pt>
2021-12-05 15:25:16 +0000

    G41-72 #time 1h #comment Adding the controller and class to execute the script in java

---
+++ src/main/java/lapr/project/data/US210_SQL.java
@@ -0,0 +1,51 @@
+package lapr.project.data;
+
+import lapr.project.controller.App;
+import lapr.project.utils.FileOperation;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Types;
+
+public class US210_SQL {
+
+    private Connection databaseConnection;
+    private String listOfShips;
+    private FileOperation fileOperation;
+
+    public US210_SQL() throws SQLException, IOException {
+        databaseConnection = App.getInstance().getDatabaseConnection().getConnection();
+        fileOperation = new FileOperation();
+    }
+
+    public void demo() throws SQLException, IOException {
+        try {
+            CallableStatement statement = databaseConnection.prepareCall("{CALL US210(?)}");
+
+
+            statement.registerOutParameter(1, Types.LONGVARCHAR);
+
+            statement.executeUpdate();
+
+            this.listOfShips = statement.getString(1);
+
+            System.out.println(statement.getString(1));
+
+            StringBuilder data = new StringBuilder();
+            data.append(listOfShips);
+            fileOperation.writeToAFile("US210", data);
+
+            statement.close();
+        }catch (Exception e){
+            StringBuilder data = new StringBuilder();
+            data.append("No results.");
+            fileOperation.writeToAFile("US210", data);
+        }
+    }
+
+
+}
