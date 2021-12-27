27700cfbe807656500a51f7f0d40e1d1b96c82ae
Hugo Nogueira <1170504@isep.ipp.pt>
2021-12-27 17:01:47 +0000

    G41-160 #time 1h #comment review US308 script

--- Documentation/Sprint3/BDDAD/US308.sql
+++ Documentation/Sprint3/BDDAD/US308.sql
@@ -1,33 +1,35 @@
--- O navio de destino está devidamente identificado.  *
--- A capacidade disponível do navio foi calculada +
--- corretamente. O aviso é acionado quando necessário +
+-- O navio de destino está devidamente identificado.
+-- A capacidade disponível do navio foi calculada 
+-- corretamente. O aviso é acionado quando necessário 
 
 
 SET SERVEROUTPUT ON;
 CREATE OR REPLACE TRIGGER trg_prevent_excess_load
-	BEFORE INSERT ON CARGOMANIFEST_LOAD
+	BEFORE INSERT OR UPDATE ON container_cargoManifest
 	FOR EACH ROW
 DECLARE
 	available_capacity_ship INTEGER;
+    sh ship.mmsi%TYPE;
 	
 CURSOR cur_capacity_available IS
-	    SELECT s.mmsi, (s.cap - (ccm.COUNT(cargo_manifest_id))) AS CAPACITY_SHIP -- COUNT(*) AS CARGO_MANIFESTS 
+	    SELECT (s.cap - (COUNT(ccm.cargo_manifest_id))) INTO available_capacity_ship
 		FROM container_cargoManifest ccm
 		INNER JOIN cargo_manifest_load cml ON ccm.cargo_manifest_id = cml.id
-		INNER JOIN ship s ON cml.mmsi = s.mmsi
-		WHERE CAPACITY_SHIP = available_capacity_ship;
+		INNER JOIN ship s ON cml.ship_mmsi = s.mmsi
+		WHERE s.mmsi = sh;
+        
 BEGIN
-	OPEN cur_capacity_available();
+	OPEN cur_capacity_available;
+        LOOP    
         FETCH cur_capacity_available
         INTO available_capacity_ship;
         EXIT WHEN cur_capacity_available%notfound;
-			IF inserting
-			THEN
+        END LOOP;
+        -- CLOSE cur_capacity_available;
+        dbms_output.put_line(available_capacity_ship);
 				IF(available_capacity_ship < 0) -- The number of cargo manifests is greater than the ship's capacity
 				THEN
 					raise_application_error(-20501, 'The ships capacity has been exceeded.');
-				END IF;
-			END IF;    
-        CLOSE cur_capacity_available;
+				END IF;           
+         CLOSE cur_capacity_available;
 END;
-/
