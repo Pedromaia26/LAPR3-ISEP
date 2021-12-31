06821c3d7dbca3466c1a63890fd02fee96459b00
Daniel Braga <1200801@isep.ipp.pt>
2021-12-31 04:03:52 +0000

    G41-111 #time 30min #comment us313 modification

--- Documentation/Sprint3/ARQCP/US313/main.c
+++ Documentation/Sprint3/ARQCP/US313/main.c
@@ -6,22 +6,9 @@ int main(){
 	int x;
 	int y;
 	int z;
-	
-	int max_x=20;
-	int max_y=20;
-	int max_z=20;
-	int cont[max_x][max_y][max_z];
-	
-	
-	int *ptr = &cont[0][0][0];
-	for(int i=0;i<max_x;i++){
-		for(int j=0;j<max_y;j++){
-			for(int k=0;k<max_z;k++){
-				cont[i][j][k]=0;
-				ptr++;
-			}
-		}
-	}
+	int cap_x;
+	int cap_y;
+	int cap_z;
 	
 	
 	file=fopen("containers.txt","r");
@@ -30,6 +17,19 @@ int main(){
 		return 1;
 	}
 	
+	fscanf(file, "%d %d %d", &cap_x, &cap_y, &cap_z);
+	int cont[cap_x][cap_y][cap_z];
+	
+	int *ptr = &cont[0][0][0];
+	for(int i=0;i<cap_x;i++){
+		for(int j=0;j<cap_y;j++){
+			for(int k=0;k<cap_z;k++){
+				cont[i][j][k]=0;
+				ptr++;
+			}
+		}
+	}
+		
 	while(!feof(file)){
 		fscanf(file, "%d %d %d %d", &id, &x, &y, &z);
 		cont[x-1][y-1][z-1]=id;
@@ -37,10 +37,10 @@ int main(){
 	
 	fclose(file);
 	
-	for(int i=0;i<max_x;i++){
+	for(int i=0;i<cap_x;i++){
 		printf("x=%d\n",i+1);
-		for(int j=0;j<max_y;j++){
-			for(int k=0;k<max_z;k++){
+		for(int j=0;j<cap_y;j++){
+			for(int k=0;k<cap_z;k++){
 				printf("%d ", cont[i][j][k]);
 				ptr++;
 			}
