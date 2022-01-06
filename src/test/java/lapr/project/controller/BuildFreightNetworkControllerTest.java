f667f658ecee7f8e523f7f54e4ba69c38f392c9c
LAPTOP-042NG862\Tiago <1201276@isep.ipp.pt>
2022-01-06 23:49:45 +0000

    G41-320 #comment improving mutation coverage

--- src/test/java/lapr/project/controller/BuildFreightNetworkControllerTest.java
+++ src/test/java/lapr/project/controller/BuildFreightNetworkControllerTest.java
@@ -1,13 +1,12 @@
 package lapr.project.controller;
 
+import lapr.project.data.BuildFreightNetworkController;
 import lapr.project.model.*;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class BuildFreightNetworkControllerTest {
 
--- src/test/java/lapr/project/model/MatrixGraphTest.java
+++ src/test/java/lapr/project/model/MatrixGraphTest.java
@@ -2,11 +2,8 @@ package lapr.project.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import lapr.project.controller.App;
-import lapr.project.controller.BuildFreightNetworkController;
+import lapr.project.data.BuildFreightNetworkController;
 import lapr.project.controller.ImportCountriesBordersSeadistsController;
-import lapr.project.model.Edge;
-import lapr.project.model.Graph;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,9 +12,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
