8199cfdf0bb21d3a96999e5a2ea2413a50d166ee
LAPTOP-042NG862\Tiago <1201276@isep.ipp.pt>
2022-01-15 21:20:15 +0000

    G41-227 #time 3h #comment adding unit tests to test the controller

--- src/test/java/lapr/project/controller/ShortestDistanceGreatestLocationCircuitControllerTest.java
+++ src/test/java/lapr/project/controller/ShortestDistanceGreatestLocationCircuitControllerTest.java
@@ -1,25 +1,125 @@
 package lapr.project.controller;
 
-import lapr.project.model.Country;
-import lapr.project.model.Graph;
-import lapr.project.model.GraphElement;
+import lapr.project.model.*;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShortestDistanceGreatestLocationCircuitControllerTest {
 
+    @BeforeEach
+    public void setup(){
+        App.getInstance().setCompany(new Company());
+        c = App.getInstance().getCompany();
+        country1 = new Country("Europa", "PT", "PRT", "Portugal", 0.5f, "Lisboa", 20, 40);
+        country2 = new Country("Europa", "ES", "ESP", "Espanha", 0.5f, "Madrid", 30, 20);
+        country3 = new Country("Europa", "FR", "FRA", "Franca", 0.5f, "Paris", 40, 30);
+        country4 = new Country("Europa", "SU", "SUI", "Suica", 0.5f, "Berna", 50, 10);
+        country5 = new Country("Europa", "GE", "GER", "Alemanha", 0.5f, "Berlim", 35, 25);
+        border1 = new Border(country1, country4);
+        border2 = new Border(country2, country1);
+        border3 = new Border(country3, country2);
+        border4 = new Border(country1, country5);
+        border5 = new Border(country4, country5);
+        seadist1 = new Seadist("Portugal", 12345, "Port1", "Espanha", 12346, "Port2", 100);
+        seadist2 = new Seadist("Portugal", 12345, "Port1", "Franca", 12346, "Port3", 200);
+        seadist3 = new Seadist("Portugal", 12345, "Port1", "Suica", 12346, "Port4", 250);
+        seadist4 = new Seadist("Portugal", 12345, "Port1", "Alemanha", 12346, "Port5", 300);
+
+    }
+
+    Company c;
+    Country country1;
+    Country country2;
+    Country country3;
+    Country country4;
+    Country country5;
+    Border border1;
+    Border border2;
+    Border border3;
+    Border border4;
+    Border border5;
+    Seadist seadist1;
+    Seadist seadist2;
+    Seadist seadist3;
+    Seadist seadist4;
+
     @Test
-    void calculateCircuit() {
-        Country country1 = new Country("Europe", "Portugal", "PT", "PRT", 10121746, "Lisboa", 20, 40);
-        Country country2 = new Country("Europe", "Portugal", "PT", "PRT", 10121746, "Lisboa", 20, 40);
-        Country country3 = new Country("Europe", "Portugal", "PT", "PRT", 10121746, "Lisboa", 20, 40);
-        Country country4 = new Country("Europe", "Portugal", "PT", "PRT", 10121746, "Lisboa", 20, 40);
-        Country country5 = new Country("Europe", "Portugal", "PT", "PRT", 10121746, "Lisboa", 20, 40);
-        GraphElement graphElement1 = new GraphElement(country1);
-        GraphElement graphElement2 = new GraphElement(country2);
-        GraphElement graphElement3 = new GraphElement(country3);
-        GraphElement graphElement4 = new GraphElement(country4);
-        GraphElement graphElement5 = new GraphElement(country5);
+    void getCircuit() throws IOException {
+        c.getCountryStore().addCountry(country1);
+        c.getCountryStore().addCountry(country2);
+        c.getCountryStore().addCountry(country3);
+        c.getCountryStore().addCountry(country4);
+        c.getCountryStore().addCountry(country5);
+        c.getBorderStore().addBorder(border1);
+        c.getBorderStore().addBorder(border2);
+        c.getBorderStore().addBorder(border3);
+        c.getBorderStore().addBorder(border4);
+        c.getBorderStore().addBorder(border5);
+        Port port1 = new Port("12345", "Port1", "Europa", "Portugal", "21", "25");
+        Port port2 = new Port("12346", "Port2", "Europa", "Espanha", "22", "26");
+        Port port3 = new Port("12347", "Port3", "Europa", "Franca", "23", "27");
+        Port port4 = new Port("12348", "Port4", "Europa", "Suica", "24", "28");
+        Port port5 = new Port("12349", "Port5", "Europa", "Alemanha", "25", "29");
+        List<KDTPort.Node<Port>> listaa = new ArrayList<>();
+        listaa.add(new KDTPort.Node<>(port1, port1.getLatitude(), port1.getLongitude()));
+        listaa.add(new KDTPort.Node<>(port2, port2.getLatitude(), port2.getLongitude()));
+        listaa.add(new KDTPort.Node<>(port3, port3.getLatitude(), port3.getLongitude()));
+        listaa.add(new KDTPort.Node<>(port4, port4.getLatitude(), port4.getLongitude()));
+        listaa.add(new KDTPort.Node<>(port5, port5.getLatitude(), port5.getLongitude()));
+        c.getKdtPorts().buildTree(listaa);
+        c.getSeadistStore().addSeadist(seadist1);
+        c.getSeadistStore().addSeadist(seadist2);
+        c.getSeadistStore().addSeadist(seadist3);
+        c.getSeadistStore().addSeadist(seadist4);
+        BuildFreightNetworkController controller = new BuildFreightNetworkController(c);
+        controller.BuildFreightNetwork(1);
+        ShortestDistanceGreatestLocationCircuitController sdglcontroller = new ShortestDistanceGreatestLocationCircuitController(c);
+        sdglcontroller.getCircuit("Paris");
+    }
+
+    @Test
+    void calculateCircuit() throws IOException {
+        c.getCountryStore().addCountry(country1);
+        c.getCountryStore().addCountry(country2);
+        c.getCountryStore().addCountry(country3);
+        c.getCountryStore().addCountry(country4);
+        c.getCountryStore().addCountry(country5);
+        c.getBorderStore().addBorder(border1);
+        c.getBorderStore().addBorder(border2);
+        c.getBorderStore().addBorder(border3);
+        c.getBorderStore().addBorder(border4);
+        c.getBorderStore().addBorder(border5);
+        Port port1 = new Port("12345", "Port1", "Europa", "Portugal", "21", "25");
+        Port port2 = new Port("12346", "Port2", "Europa", "Espanha", "22", "26");
+        Port port3 = new Port("12347", "Port3", "Europa", "Franca", "23", "27");
+        Port port4 = new Port("12348", "Port4", "Europa", "Suica", "24", "28");
+        Port port5 = new Port("12349", "Port5", "Europa", "Alemanha", "25", "29");
+        List<KDTPort.Node<Port>> listaa = new ArrayList<>();
+        listaa.add(new KDTPort.Node<>(port1, port1.getLatitude(), port1.getLongitude()));
+        listaa.add(new KDTPort.Node<>(port2, port2.getLatitude(), port2.getLongitude()));
+        listaa.add(new KDTPort.Node<>(port3, port3.getLatitude(), port3.getLongitude()));
+        listaa.add(new KDTPort.Node<>(port4, port4.getLatitude(), port4.getLongitude()));
+        listaa.add(new KDTPort.Node<>(port5, port5.getLatitude(), port5.getLongitude()));
+        c.getKdtPorts().buildTree(listaa);
+        c.getSeadistStore().addSeadist(seadist1);
+        c.getSeadistStore().addSeadist(seadist2);
+        c.getSeadistStore().addSeadist(seadist3);
+        c.getSeadistStore().addSeadist(seadist4);
+        BuildFreightNetworkController controller = new BuildFreightNetworkController(c);
+        controller.BuildFreightNetwork(1);
+        ShortestDistanceGreatestLocationCircuitController sdglcontroller = new ShortestDistanceGreatestLocationCircuitController(c);
+        List<Edge> listaEdge = sdglcontroller.calculateCircuit(new GraphElement(port1));
+        List<Edge> expected = new ArrayList<>();
+        expected.add(new Edge(new GraphElement(port1), new GraphElement(port2), 100.00));
+        expected.add(new Edge(new GraphElement(port2), new GraphElement(country2), 1072.30));
+        expected.add(new Edge(new GraphElement(country2), new GraphElement(port1), 1172.30));
+        assertEquals(expected, listaEdge);
     }
 }
--- src/test/java/lapr/project/model/MatrixGraphTest.java
+++ src/test/java/lapr/project/model/MatrixGraphTest.java
@@ -607,15 +607,93 @@ public class MatrixGraphTest {
 
     @Test
     void transitiveClosure() throws IOException {
+        Country country1 = new Country("Europa", "PT", "PRT", "Portugal", 0.5f, "Lisboa", 20, 40);
+        Country country2 = new Country("Europa", "ES", "ESP", "Spain", 0.5f, "Madrid", 30, 20);
+        Country country3 = new Country("Europa", "FR", "FRA", "France", 0.5f, "Paris", 40, 30);
+        Country country4 = new Country("Europa", "SU", "SUI", "Switzerland", 0.5f, "Berna", 50, 10);
+        Country country5 = new Country("Europa", "GE", "GER", "Germany", 0.5f, "Berlim", 35, 25);
+        Country country6 = new Country("America", "CL", "CHL", "Chile", 16.8f, "Santiago", -33.45f, -70.666667f);
+        ArrayList<GraphElement> listGraph = new ArrayList<>();
+        listGraph.add(new GraphElement(country1));
+        listGraph.add(new GraphElement(country2));
+        listGraph.add(new GraphElement(country3));
+        listGraph.add(new GraphElement(country4));
+        listGraph.add(new GraphElement(country5));
+        listGraph.add(new GraphElement(country6));
+        Object[][] m = {{10d, 7d, 5d, 1d, 1d, 2d}, {1d, 1d, 1d, 1d, 1d, 1d}, {1d, 1d, 1d, 1d, 1d, 1d}, {1d, 1d, 1d, 1d, 1d, 1d}, {1d, 1d, 1d, 1d, 1d, 1d}, {1d, 1d, 1d, 1d, 1d, 1d}};
+        MatrixGraph matrix = new MatrixGraph(true, listGraph, m);
         ImportCountriesBordersSeadistsController icontroller = new ImportCountriesBordersSeadistsController();
-        BuildFreightNetworkController bfnc = new BuildFreightNetworkController();
         icontroller.importFromDatabaseCountries();
         icontroller.importFromDatabaseBorders();
         icontroller.importFromDatabaseSeadists();
-        bfnc.BuildFreightNetwork(3);
+        matrix.transitiveClosure(matrix);
+    }
+
+
+    @Test
+    void dijkstra() {
+        List<Integer> list;
+        List<Integer> list2 = new ArrayList<>();
+        Country country1 = new Country("Europa", "PT", "PRT", "Portugal", 0.5f, "Lisboa", 20, 40);
+        Country country2 = new Country("Europa", "ES", "ESP", "Espanha", 0.5f, "Madrid", 30, 20);
+        Country country3 = new Country("Europa", "FR", "FRA", "Franca", 0.5f, "Paris", 40, 30);
+        Country country4 = new Country("Europa", "SU", "SUI", "Suica", 0.5f, "Berna", 50, 10);
+        Country country5 = new Country("Europa", "GE", "GER", "Alemanha", 0.5f, "Berlim", 35, 25);
+        ArrayList<GraphElement> listGraph = new ArrayList<>();
+        listGraph.add(new GraphElement(country1));
+        listGraph.add(new GraphElement(country2));
+        listGraph.add(new GraphElement(country3));
+        listGraph.add(new GraphElement(country4));
+        listGraph.add(new GraphElement(country5));
+        Object[][] m = {{10d, 7d, 5d, 1d, 1d}, {1d, 1d, 1d, 1d, 1d}, {1d, 1d, 1d, 1d, 1d}, {1d, 1d, 1d, 1d, 1d}, {1d, 1d, 1d, 1d, 1d}};
+        MatrixGraph matrix = new MatrixGraph(true, listGraph, m);
+        list = matrix.dijkstra(matrix, "Paris", "Lisboa", 3);
+        list2.add(2);
+        list2.add(0);
+        assertEquals(list, list2);
     }
 
     @Test
-    void shortestPath() {
+    void dijkstraPathNotFound() {
+        List<Integer> list;
+        List<Integer> list2 = new ArrayList<>();
+        Country country1 = new Country("Europa", "PT", "PRT", "Portugal", 0.5f, "Lisboa", 20, 40);
+        Country country2 = new Country("Europa", "ES", "ESP", "Espanha", 0.5f, "Madrid", 30, 20);
+        Country country3 = new Country("Europa", "FR", "FRA", "Franca", 0.5f, "Paris", 40, 30);
+        Country country4 = new Country("Europa", "SU", "SUI", "Suica", 0.5f, "Berna", 50, 10);
+        Country country5 = new Country("Europa", "GE", "GER", "Alemanha", 0.5f, "Berlim", 35, 25);
+        ArrayList<GraphElement> listGraph = new ArrayList<>();
+        listGraph.add(new GraphElement(country1));
+        listGraph.add(new GraphElement(country2));
+        listGraph.add(new GraphElement(country3));
+        listGraph.add(new GraphElement(country4));
+        listGraph.add(new GraphElement(country5));
+        Object[][] m = {{10d, 7d, 5d, 1d, 1d}, {1d, 1d, 1d, 1d, 1d}, {1d, 1d, 1d, 1d, 1d}, {1d, 1d, 1d, 1d, 1d}, {1d, 1d, 1d, 1d, 1d}};
+        MatrixGraph matrix = new MatrixGraph(true, listGraph, m);
+        list = matrix.dijkstra(matrix, "Paris", "Lisboa", 2);
+        assertEquals(list, list2);
+    }
+
+    @Test
+    void dijkstraLandPath() {
+        List<Integer> list;
+        List<Integer> list2 = new ArrayList<>();
+        Country country1 = new Country("Europa", "PT", "PRT", "Portugal", 0.5f, "Lisboa", 20, 40);
+        Country country2 = new Country("Europa", "ES", "ESP", "Espanha", 0.5f, "Madrid", 30, 20);
+        Country country3 = new Country("Europa", "FR", "FRA", "Franca", 0.5f, "Paris", 40, 30);
+        Country country4 = new Country("Europa", "SU", "SUI", "Suica", 0.5f, "Berna", 50, 10);
+        Country country5 = new Country("Europa", "GE", "GER", "Alemanha", 0.5f, "Berlim", 35, 25);
+        ArrayList<GraphElement> listGraph = new ArrayList<>();
+        listGraph.add(new GraphElement(country1));
+        listGraph.add(new GraphElement(country2));
+        listGraph.add(new GraphElement(country3));
+        listGraph.add(new GraphElement(country4));
+        listGraph.add(new GraphElement(country5));
+        Object[][] m = {{10d, 7d, 5d, 1d, 1d}, {1d, 1d, 1d, 1d, 1d}, {1d, 1d, 1d, 1d, 1d}, {1d, 1d, 1d, 1d, 1d}, {1d, 1d, 1d, 1d, 1d}};
+        MatrixGraph matrix = new MatrixGraph(true, listGraph, m);
+        list = matrix.dijkstra(matrix, "Paris", "Lisboa", 1);
+        list2.add(2);
+        list2.add(0);
+        assertEquals(list, list2);
     }
 }
