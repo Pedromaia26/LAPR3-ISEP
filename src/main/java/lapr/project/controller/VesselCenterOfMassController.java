97d0b082a80270adcca9a72abc032ae858f2f276
Pedro_Maia <1201381@isep.ipp.pt>
2022-01-22 02:07:38 +0000

    G41-229 #time 2h 30min #comment Adding the controller and java class to perform the us [Implementation]

---
+++ src/main/java/lapr/project/controller/VesselCenterOfMassController.java
@@ -0,0 +1,39 @@
+package lapr.project.controller;
+
+import lapr.project.data.Physics.VesselCenterOfMass;
+import lapr.project.utils.FileOperation;
+
+import java.io.IOException;
+
+public class VesselCenterOfMassController {
+
+    private StringBuilder data;
+
+    public VesselCenterOfMassController() {
+        data = new StringBuilder();
+    }
+
+    public void getCenterOfMass(String typeOfShip) throws IOException {
+        try{
+            VesselCenterOfMass centerOfMass = new VesselCenterOfMass(typeOfShip);
+            data.append("--- Center of mass of the " + typeOfShip + " ---\n");
+            data.append("X Value: " + String.format("%.2f",centerOfMass.getCenterOfMassX()) + "m\n" +
+                    "Y Value: " + String.format("%.2f",centerOfMass.getCenterOfMassY()) + "m\n" +
+                    "\n--- Vessel Measurements ---\n" +
+                    "Vessel Length: " + String.format("%.2f",centerOfMass.getVesselLength()) + "m\n" +
+                    "Vessel Width: " + String.format("%.2f",centerOfMass.getVesselWidth()) + "m\n" +
+                    "Vessel Height: " + String.format("%.2f",centerOfMass.getVesselHeight()) + "m\n" +
+                    "Vessel Tip Length: " + String.format("%.2f",centerOfMass.getVesselTipLength()) + "m\n" +
+                    "Vessel Tip Height: " + String.format("%.2f",centerOfMass.getVesselTipHeight()) + "m\n" +
+                    "Vessel Body Length: " + String.format("%.2f",centerOfMass.getVesselBodyLength()) + "m\n" +
+                    "Vessel Body Height: " + String.format("%.2f",centerOfMass.getVesselBodyHeight()) + "m\n" +
+                    "Vessel Tower Length: " + String.format("%.2f",centerOfMass.getVesselTowerLength()) + "m\n" +
+                    "Vessel Tower Height: " + String.format("%.2f",centerOfMass.getVesselTowerHeight()) + "m\n" +
+                    "Vessel Material Density: " + String.format("%.2f",centerOfMass.getMATERIAL_DENSITY()) + "g/cm3\n");
+        }
+        catch (IllegalArgumentException e){
+            data.append("The vessel type is invalid.\n");
+        }
+        FileOperation.writeToAFile("Output/US418.txt", data);
+    }
+}
---
+++ src/main/java/lapr/project/data/Physics/VesselCenterOfMass.java
@@ -0,0 +1,169 @@
+package lapr.project.data.Physics;
+
+public class VesselCenterOfMass {
+
+    private double vesselLength;
+    private double vesselWidth;
+    private double vesselHeight;
+    private double vesselTipLength;
+    private double vesselTipHeight;
+    private double vesselBodyLength;
+    private double vesselBodyHeight;
+    private double vesselTowerLength;
+    private double vesselTowerHeight;
+    private final double MATERIAL_DENSITY=7.8d;
+    private double centerOfMassX;
+    private double centerOfMassY;
+
+    public VesselCenterOfMass(String typeOfVessel){
+        switch(typeOfVessel){
+            case "Container Ship":
+                vesselLength = 390;
+                vesselWidth = 50;
+                vesselHeight = 30;
+                vesselTipLength = 30;
+                vesselTipHeight = 30;
+                vesselBodyLength= 330;
+                vesselBodyHeight = 30;
+                vesselTowerLength = 20;
+                vesselTowerHeight = 5;
+                centerOfMassX = calculateCenterOfMassXContainer();
+                break;
+            case "Oil Tanker":
+                vesselLength = 250;
+                vesselWidth = 44;
+                vesselHeight = 25;
+                vesselTipLength = 25;
+                vesselTipHeight = 25;
+                vesselBodyLength= 200;
+                vesselBodyHeight = 25;
+                vesselTowerLength = 20;
+                vesselTowerHeight = 5;
+                centerOfMassX = calculateCenterOfMassXOil();
+                break;
+            case "Bulk Carrier":
+                vesselLength = 362;
+                vesselWidth = 65;
+                vesselHeight = 56;
+                vesselTipLength = 56;
+                vesselTipHeight = 56;
+                vesselBodyLength= 250;
+                vesselBodyHeight = 56;
+                vesselTowerLength = 20;
+                vesselTowerHeight = 5;
+                centerOfMassX = calculateCenterOfMassXBulk();
+                break;
+            default:
+                throw new IllegalArgumentException("The vessel type is not valid");
+        }
+        centerOfMassY = calculateCenterOfMassY();
+    }
+
+    private double calculateCenterOfMassXContainer(){
+        double tipVolume = getTipVolume();
+        double bodyVolume = getBodyVolume();
+        double towerVolume = getTowerVolume();
+        double tipMass = getMass(tipVolume);
+        double bodyMass = getMass(bodyVolume);
+        double towerMass = getMass(towerVolume);
+        double centerOfMassX = (tipMass*(vesselHeight/3) + bodyMass*(vesselLength/2) + tipMass*(vesselLength-vesselHeight/3) + towerMass*(vesselLength/2))/(tipMass*2 + bodyMass + towerMass);
+        return centerOfMassX;
+    }
+
+    private double calculateCenterOfMassXOil(){
+        double tipVolume = getTipVolume();
+        double bodyVolume = getBodyVolume();
+        double towerVolume = getTowerVolume();
+        double tipMass = getMass(tipVolume);
+        double bodyMass = getMass(bodyVolume);
+        double towerMass = getMass(towerVolume);
+        double centerOfMassX = (tipMass*(vesselHeight/3) + bodyMass*(vesselLength/2) + tipMass*(vesselLength-vesselHeight/3) + towerMass*(20))/(tipMass*2 + bodyMass + towerMass);
+        return centerOfMassX;
+    }
+
+    private double calculateCenterOfMassXBulk(){
+        double tipVolume = getTipVolume();
+        double bodyVolume = getBodyVolume();
+        double towerVolume = getTowerVolume();
+        double tipMass = getMass(tipVolume);
+        double bodyMass = getMass(bodyVolume);
+        double towerMass = getMass(towerVolume);
+        double centerOfMassX = (tipMass*(vesselHeight/3) + bodyMass*(vesselLength/2) + tipMass*(vesselLength-vesselHeight/3) + towerMass*(330))/(tipMass*2 + bodyMass + towerMass);
+        return centerOfMassX;
+    }
+
+    private double calculateCenterOfMassY(){
+        double tipVolume = getTipVolume();
+        double bodyVolume = getBodyVolume();
+        double towerVolume = getTowerVolume();
+        double tipMass = getMass(tipVolume);
+        double bodyMass = getMass(bodyVolume);
+        double towerMass = getMass(towerVolume);
+        double centerOfMassY = (tipMass*(vesselTipHeight-vesselHeight/3)*2 + bodyMass*(vesselHeight/2) + towerMass*(vesselHeight + vesselTowerHeight/2))/(tipMass*2 + bodyMass + towerMass);
+        return centerOfMassY;
+    }
+
+    public double getMass(double volume){
+        return volume*MATERIAL_DENSITY;
+    }
+
+    public double getTipVolume(){
+        return vesselTipHeight*vesselHeight;
+    }
+
+    public double getBodyVolume(){
+        return vesselWidth*vesselBodyLength*vesselHeight;
+    }
+
+    public double getTowerVolume(){
+        return vesselTowerHeight*vesselTowerLength*vesselWidth;
+    }
+
+    public double getCenterOfMassX(){
+        return centerOfMassX;
+    }
+
+    public double getCenterOfMassY() {
+        return centerOfMassY;
+    }
+
+    public double getVesselLength() {
+        return vesselLength;
+    }
+
+    public double getVesselWidth() {
+        return vesselWidth;
+    }
+
+    public double getVesselHeight() {
+        return vesselHeight;
+    }
+
+    public double getVesselTipLength() {
+        return vesselTipLength;
+    }
+
+    public double getVesselTipHeight() {
+        return vesselTipHeight;
+    }
+
+    public double getVesselBodyLength() {
+        return vesselBodyLength;
+    }
+
+    public double getVesselBodyHeight() {
+        return vesselBodyHeight;
+    }
+
+    public double getVesselTowerLength() {
+        return vesselTowerLength;
+    }
+
+    public double getVesselTowerHeight() {
+        return vesselTowerHeight;
+    }
+
+    public double getMATERIAL_DENSITY() {
+        return MATERIAL_DENSITY;
+    }
+}
